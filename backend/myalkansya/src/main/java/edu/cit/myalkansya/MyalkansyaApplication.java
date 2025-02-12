2a69d39b7b6baa43e4c995603f2ff0ac45c82f18
LAB14_USER ON WS24 <L14X08W24@citulab.net>
2025-02-12 10:02:37 +0800

    Added backend springboot files

---
+++ backend/myalkansya/src/main/java/edu/cit/myalkansya/MyalkansyaApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.myalkansya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyalkansyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyalkansyaApplication.class, args);
+	}
+
+}
---
+++ backend/myalkansya/src/test/java/edu/cit/myalkansya/MyalkansyaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.myalkansya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyalkansyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
