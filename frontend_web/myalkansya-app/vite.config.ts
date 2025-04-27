2312f1ce7b2496dc0ed35ad91d3ca490eb1f0995
WazeyBoi <business.xianprs@gmail.com>
2025-04-27 21:14:59 +0800

    Revert Vercel changewq

--- frontend_web/myalkansya-app/vite.config.ts
+++ frontend_web/myalkansya-app/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/IT342-MyAlkansya',
   plugins: [
     tailwindcss(),
   ],
